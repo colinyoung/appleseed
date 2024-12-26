e88f8f82fd5d9929c27ed960ae55868e03dbe0e8
Colin Young <me@colinyoung.com>
2024-12-25 20:37:35 -0600

    Prep for build

--- db.ts
+++ db.ts
@@ -7,12 +7,7 @@ const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl:
-    process.env.NODE_ENV === 'production'
-      ? {
-          rejectUnauthorized: false,
-        }
-      : undefined,
+  ssl: { rejectUnauthorized: false },
 });
 
 export async function query(text: string, params: any[]): Promise<QueryResult> {
--- migrate.ts
+++ migrate.ts
@@ -1,4 +1,4 @@
-import { query } from './db.js';
+import { query } from './db';
 import fs from 'fs';
 import { parse } from 'csv-parse';
 
