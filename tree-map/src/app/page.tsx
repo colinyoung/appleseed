bffadbaca271de6c17c49b329fc77514b2f3987e
Colin Young <me@colinyoung.com>
2024-12-27 15:23:23 -0600

    Add suspense

--- tree-map/src/app/page.tsx
+++ tree-map/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import Content from './_content';
 import { TreeMapContextProvider } from './_context';
 import { getMarkers } from './_functions';
@@ -7,9 +8,11 @@ export default async function Home() {
 
   return (
     <main className="flex-1 flex-col h-screen w-full">
-      <TreeMapContextProvider markers={markers}>
-        <Content />
-      </TreeMapContextProvider>
+      <Suspense fallback={<div>Loading...</div>}>
+        <TreeMapContextProvider markers={markers}>
+          <Content />
+        </TreeMapContextProvider>
+      </Suspense>
     </main>
   );
 }
