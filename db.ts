a00e2714ad9ac19c5ce3fb4952d86e7ec13e9c4b
Colin Young <me@colinyoung.com>
2024-12-26 23:23:25 -0600

    Allow ssl rejectunauthorized for Heroku in prod

--- db.ts
+++ db.ts
@@ -7,6 +7,11 @@ const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ...(process.env.NODE_ENV === 'production' && {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  }),
 });
 
 export type DB = {
--- tree-map/next.config.ts
+++ tree-map/next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   env: {
     GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+    NODE_ENV: process.env.NODE_ENV,
   },
 };
 
